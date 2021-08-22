bf31559172ac58fb852ab8b1915c04ef1e7f0b40
Abdelhamid Bendali <abdelhamidbendali97@gmail.com>
2021-08-22 18:09:05 +0100

    add mvc project to repository

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PHP-Custom-MVC
 Complete custom MVC (Model View Controller) framework
+
+# This framework will include
+* A core library class to load controllers & methods from the URL (Also using .htaccess)
+* A base controller class to load models and views
+* A custom database library using PDO for all models to interact with the database using prepared statements
---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '__YOURUSER__');
+    define('DB_PASS', '__YOURPASS');
+    define('DB_NAME', '__YOURDBNAME__');
+    // APPROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL ROOT
+    define('URLROOT','__YOURROOOT__');
+
+    //SITE NAME
+    define('SITENAME', '__WEBSITENAME__');
